a8f1bceb7ae5ceeb285db129a874e8ae82178e26
kaixuan.xu <kaixuan.xu@ly.com>
2022-04-29 15:45:57 +0800

    modify formula

--- Formula/curl2httpie.rb
+++ Formula/curl2httpie.rb
@@ -1,7 +1,7 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-class Curl2Httpie < Formula
+class Curl2httpie < Formula
   desc ""
   homepage ""
   url "https://github.com/azouever/homebrew-tap/releases/download/v0.0.1/curl2httpie_0.0.1.tar.gz"
